9056c398cef764270310dfd22b1f6d14b4e4ed59
lobzhanidzee <daviti.lobzhanidze.1@btu.edu.ge>
2024-09-18 17:05:32 +0400

    appart object

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,3 +3,20 @@ import { API_URL } from "./config.js";
 
 const { ...realEstates } = await AJAX(`${API_URL}real-estates`);
 console.log(realEstates);
+
+const createAppartObject = function (data) {
+  const { ...appart } = data;
+  console.log(data, appart);
+  return {
+    id: appart.id,
+    address: appart.address,
+    area: appart.area,
+    image: appart.image,
+    isRental: appart.is_rental,
+    price: appart.price,
+    zipCode: appart.zip_code,
+    ...(appart.key && { key: appart.key }),
+  };
+};
+
+console.log(createAppartObject(realEstates));
