12b605af5b4210292b0038caff5579c1c208f905
lobzhanidzee <daviti.lobzhanidze.1@btu.edu.ge>
2024-10-05 00:05:15 +0400

    selected filter fix

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,6 +2,7 @@ import HomeView from "./views/homeView.js";
 import AgentModalView from "./views/agentModalView.js";
 import FiltersView from "./views/filtersView.js";
 import selectedFilterView from "./views/selectedFilterView.js";
+import { AJAX } from "./helpers.js";
 
 if (window.location.pathname.endsWith("/")) {
   HomeView.GetAppartments();
@@ -10,3 +11,18 @@ if (window.location.pathname.endsWith("/")) {
 
 FiltersView.render();
 selectedFilterView.getSelectFilters();
+
+const regions = await AJAX(
+  "https://api.real-estate-manager.redberryinternship.ge/api/regions"
+);
+
+export function getRegionNameById(id) {
+  const region = regions.find((region) => region.id === id);
+  return region ? region.name : null;
+}
+
+// Example usage
+const regionName = getRegionNameById(1);
+console.log(regionName);
+
+console.log(regions);
--- src/js/views/selectedFilterView.js
+++ src/js/views/selectedFilterView.js
@@ -1,8 +1,15 @@
+import { getRegionNameById } from "../controller.js";
+
 class SelectedFiltersView {
   _regionFilterForm = document.querySelector(".region-filter-form");
+  _cleanBtn = document.querySelector(".filter__container--btn");
+
+  constructor() {
+    this._cleanBtn.addEventListener("click", this._clearFilters.bind(this));
+  }
 
   getSelectFilters() {
-    this._regionFilterForm.addEventListener("submit", (event) => {
+    this._regionFilterForm.addEventListener("submit", async (event) => {
       event.preventDefault();
       const parentEl = document.querySelector(".filter__container");
       const storedInputs = [];
@@ -12,19 +19,52 @@ class SelectedFiltersView {
         storedInputs.push({ key, value });
       }
 
-      const html = storedInputs
-        .map(
-          (input) => `
+      const existingValues = new Set(
+        Array.from(
+          parentEl.querySelectorAll(".filter__container--item span")
+        ).map((span) => span.textContent)
+      );
+
+      const newValues = new Set(
+        storedInputs.map((input) => getRegionNameById(+input.value))
+      );
+
+      parentEl.querySelectorAll(".filter__container--item").forEach((item) => {
+        const spanText = item.querySelector("span").textContent;
+        if (!newValues.has(spanText)) {
+          item.remove();
+        }
+      });
+
+      let html = "";
+      storedInputs
+        .filter((input) => !existingValues.has(getRegionNameById(+input.value)))
+        .forEach((input) => {
+          html += `
           <div class="filter__container--item text-icon">
-            <span>${input.value}</span>
-            <img src="./src/img/icons/x-icon.svg" alt="remove icon" class="btn" />
-          </div>
-        `
-        )
-        .join("");
+                <span>${getRegionNameById(+input.value)}</span>
+                <img src="./src/img/icons/x-icon.svg" alt="remove icon" class="btn" />
+            </div>`;
+          this._cleanBtn.classList.remove("hidden");
+        });
 
       parentEl.insertAdjacentHTML("afterbegin", html);
+
+      if (!html) {
+        this._cleanBtn.classList.add("hidden");
+      }
     });
+  }
+
+  _clearFilters() {
+    const parentEl = document.querySelector(".filter__container");
+    Array.from(parentEl.children).forEach((child) => {
+      if (!child.classList.contains("filter__container--btn")) {
+        child.remove();
+      }
+    });
+    this._regionFilterForm.reset(); // Reset the form selections
+    this._cleanBtn.classList.add("hidden");
   }
 }
 
